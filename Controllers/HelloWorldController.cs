617f73959221d2fd9d7c003e0e749b42dc11303b
rootslowed <rootslowed@gmail.com>
2021-09-07 15:34:26 -0300

    initial files

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+  public class HelloWorldController
+  {
+    public string Index()
+    {
+      return "This is my default action...";
+    }
+
+    public string Welcome(string name, int numTimes = 1)
+    {
+      return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+  }
+}
