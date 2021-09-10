bb96b6489d10225d19f90098230a888f71dd2c18
rootslowed <rootslowed@gmail.com>
2021-09-10 10:53:00 -0300

    initialize the seed with try catch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+using System;
 
 namespace MvcMovie
 {
@@ -7,11 +11,29 @@ namespace MvcMovie
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      var host = CreateHostBuilder(args).Build();
+
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+
+        try
+        {
+          SeedData.Initialize(services);
+        }
+        catch (Exception ex)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occurred seeding the DB.");
+        }
+      }
+
+      host.Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
       Host.CreateDefaultBuilder(args)
         .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    
   }
 }
